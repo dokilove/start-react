d9de0574373e1f3cbefb68a4dcfe94e4e5b913e4
dokilove <38000425+dokilove@users.noreply.github.com>
2022-02-21 14:45:36 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("reni run all the time");
+  const iRunOnlyOnce = ()=>{
+    console.log('i run only once');
+  };
+  useEffect(() => {
+    console.log("Call The API");
+  }, []);
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
